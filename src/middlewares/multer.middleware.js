d9b227a20b9d8e307a43e12c964c73d27931d746
swarnava chakraborty <swarnavachakraborty2@gmail.com>
2026-07-30 20:34:59 +0530

    added multer middleware

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,21 @@
+const multer = require('multer')
+
+
+//configure disk where file to be uploaded 
+const storage = multer.diskStorage({
+
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp")//stores all the uploaded files in the public/temp file
+  },
+  
+  filename: function (req, file, cb) {
+    cb(null, file.originalname) //saves the files by their original name
+  }
+})
+
+
+const upload = multer({storage: storage})
+//const upload = multer({storage: multer.memoryStorage()}) ** large files cannot be stored in memory **
+
+
+module.exports = upload
